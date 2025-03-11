dd759ea583f7323ed87617a694a4a0177e3ecaf2
Javier Terán González <javier.teran@educantabria.es>
2025-03-11 12:34:29 +0100

    Quitar Random. Hay Id de instancia

--- menu.php
+++ menu.php
@@ -12,5 +12,6 @@
     <li class="nav-item"><a class="nav-link<?php if($pagina=="/index.php"){  echo ' active'; }?>" href="index.php">Personas</a></li>
     <li class="nav-item"><a class="nav-link<?php if($pagina=="/alumnos.php"){  echo ' active'; }?>" href="alumnos.php">Alumnos</a></li>
     <li class="nav-item"><a class="nav-link<?php if($pagina=="/clientes.php"){   echo ' active'; }?>" href="clientes.php">Clientes</a></li>
-    <li class="nav-item"><a class="nav-link<?php if($pagina=="/info.php"){   echo ' active'; }?>" href="info.php"><b><?php echo $NumServidor. " ID " .$instance_id; ?></b></a></li>
+    <li class="nav-item"><a class="nav-link<?php if($pagina=="/info.php"){   echo ' active'; }?>" href="info.php"><b><?php echo $instance_id; ?></b></a></li>
+    <!-- podía poner echo $NumServidor. " ID " .$instance_id -->
 </ul>
